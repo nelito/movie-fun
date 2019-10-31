b8f812c9b53bf7bc7e5b7cd487765e3c036f8dd6
Manuel Luis <manuel.a.luis@gmail.com>
2019-10-31 09:25:51 +0000

    Datasource

--- src/main/java/org/superbiz/moviefun/HomeController.java
+++ src/main/java/org/superbiz/moviefun/HomeController.java
@@ -2,10 +2,7 @@ package org.superbiz.moviefun;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
-import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.superbiz.moviefun.albums.Album;
 import org.superbiz.moviefun.albums.AlbumFixtures;
