3d66320142121bf4b4090e18bd1cfde8f5037a0f
nelito <manuel.a.luis@gmail.com>
2019-11-01 16:51:38 +0000

    Update DBStore.java

--- src/main/java/org/superbiz/moviefun/blobstore/DBStore.java
+++ src/main/java/org/superbiz/moviefun/blobstore/DBStore.java
@@ -1,10 +1,16 @@
 package org.superbiz.moviefun.blobstore;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Optional;
 
+@Component
+@ConditionalOnProperty(value ="blobStore", havingValue = "DBStore")
 public class DBStore implements BlobStore {
 
     @Autowired
@@ -12,15 +18,16 @@ public class DBStore implements BlobStore {
 
     @Override
     public void put(Blob blob) throws IOException {
-        BlobEntity
-        blobRepository.save(blob);
+        BlobEntity blobEntity = new BlobEntity(blob.getName(), IOUtils.toByteArray(blob.getInputStream()),blob.getContentType());
+        blobRepository.save(blobEntity);
     }
 
     @Override
     public Optional<Blob> get(String name) throws IOException {
-        BlobEntity blob = blobRepository.find(name);
+        BlobEntity blobEntity = blobRepository.find(name);
         Optional<Blob> optionalBlob;
-        if (blob != null) {
+        if (blobEntity != null) {
+            Blob blob = new Blob(blobEntity.getName(), new ByteArrayInputStream(blobEntity.getBlob()),blobEntity.getContentType());
             optionalBlob = Optional.of(blob);
         } else {
             optionalBlob = Optional.empty();
@@ -31,6 +38,6 @@ public class DBStore implements BlobStore {
 
     @Override
     public void deleteAll() {
-
+        blobRepository.deleteAll();
     }
 }
